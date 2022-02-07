11f7695f13f61d167929b7202bd72273a92d6618
ShishirRijal <ccrrizal0892@gmail.com>
2022-02-07 13:17:27 +0545

    some dummy news added

--- lib/utils/dummy_news.dart
+++ lib/utils/dummy_news.dart
@@ -14,6 +14,7 @@ For instance, in the wake of the 2016 election, Trump claimed to have had higher
 ''',
     date: DateTime(2022, 01, 25),
     read: 5,
+    isTrending: true,
   ),
   News(
     id: '20220127-Teachingisload',
@@ -29,6 +30,7 @@ Institutional culture seemed to agree with me. I was given no formal instruction
 ''',
     date: DateTime(2022, 01, 27),
     read: 8,
+    isTrending: true,
   ),
   News(
     id: '20220129-Outliveyourmoney',
@@ -59,6 +61,7 @@ The finale of Miss World 2021 was postponed due to COVID-19 cases amongst the co
 ''',
     date: DateTime(2022, 02, 01),
     read: 2,
+    isTrending: true,
   ),
   News(
     id: '20220204-LeonardoDainciMother',
@@ -97,6 +100,7 @@ Meanwhile, Google is also taking time to highlight their partnership with Flame,
 ''',
     date: DateTime(2022, 02, 07),
     read: 12,
+    isTrending: true,
   ),
   News(
     id: '20220211-Breakfastaroundglobe',
@@ -117,5 +121,42 @@ Fish such as salmon or mackerel, miso soup, pickled vegetables and rice are all 
 ''',
     date: DateTime(2022, 02, 11),
     read: 15,
+  ),
+  News(
+    id: '20220213-Cancer',
+    imageAddress:
+        'https://media.nature.com/lw800/magazine-assets/d41586-022-00241-0/d41586-022-00241-0_20092060.jpg',
+    title:
+        "Last-resort cancer therapy holds back disease for more than a decade",
+    text:
+        '''A few weeks after receiving an experimental cancer therapy that turns immune cells into tumour-killing hunters, Doug Olson’s doctor sat him down to give him news of his progress. “He said, ‘Doug, we cannot find a single cancer cell in your body,’” Olson recalls. “I was pretty convinced that I was done with cancer.”
+Olson’s doctors, however, weren’t so sure. The year was 2010, and Olson was one of the first people with chronic lymphocytic leukaemia to receive the treatment, called CAR-T-cell therapy. When his doctors — including Carl June and David Porter at the University of Pennsylvania in Philadelphia — wrote the protocol for the clinical trial that Olson was involved in, they hoped that the genetically engineered cells might survive for a month in his body. They knew that cancer research could be heartbreaking; they didn’t dare to expect a cure.
+But more than ten years later, the immune cells continue to patrol Olson’s blood and he remains in remission. June is finally ready to admit what Olson suspected all along. “We can now conclude that CAR T cells can actually cure patients with leukaemia,” June told reporters at a press briefing describing results that were published in Nature on 2 February1.
+CAR-T-cell therapies involve removing immune cells called T cells from a person with cancer, and genetically altering them so that they produce proteins — called chimeric antigen receptors, or CARs — that recognize cancer cells. The cells are then reinfused into the person, in the hope that they will seek out and destroy tumours.
+
+In the years since Olson’s treatment, five CAR-T-cell therapies have been approved by the US Food and Drug Administration, to treat leukaemias, lymphomas and myelomas. June estimates that tens of thousands of people have received CAR-T cell treatment.
+But the therapy is expensive, risky and technically demanding. It remains a last resort, to be used when all other treatments have failed. Despite the treatment’s success for Olson, not everyone experiences durable remission of their cancer. In the beginning, only about 25–35% of CAR-T-cell recipients with chronic lymphocytic leukaemia experienced a complete remission of their cancer, says Porter. With refinement, that percentage has increased over the years, he says, but some of these initial successes still lead to relapse. Tracking the treatment long-term could reveal clues as to what factors are important for lasting CAR-T-cell success.
+For more than ten years, Porter and his colleagues analysed the CAR T cells in Olson and one other person treated in 2010, tracing the cells’ evolution and looking for any signs of safety concerns.
+They found that the CAR T cells persisted, but the characteristics of the population shifted over time. Soon after infusion, a prominent population of T cells called CD8+ cells emerged. These are sometimes called killer T cells, and can identify and destroy cells that display unusual proteins, such as cancer cells or cells that are infected with a virus.
+But over the years, a different type of CAR T cell became dominant. CD4+ T cells can take on a variety of functions in the immune system, but the researchers showed that both study participants had CD4+ cells with characteristics suggesting that they would be capable of killing leukaemia cells.
+''',
+    date: DateTime(2022, 02, 13),
+    read: 12,
+    isTrending: true,
+  ),
+  News(
+    id: '20220214-redminote11pro',
+    imageAddress:
+        'https://www.notebookcheck.net/fileadmin/_processed_/7/1/csm_redmi_note_11_pro_3_2_2c386107f2.png',
+    title:
+        "Redmi Note 11 Pro 5G vs Redmi Note 10 Pro: The Snapdragon 695 offers a decent generational performance boost",
+    text:
+        '''After releasing the Redmi Note 11 series in China months ago, Xiaomi finally brought the phones over to the global market, but with different hardware. The Redmi Note 11 Pro 5G, for example, trading in a MediaTek Dimensity 920 for a Snapdragon 695. That, on paper, looks like a downgrade on the Redmi Note 10 Pro's Snapdragon 732G. As we saw with the Snapdragon 678 and Redmi Note 11's Snapdragon 680, though, looks can be deceiving.
+Starting with Geekbench, the Redmi Note 11 Pro 5G comes away with a score of 675 in the single-core test. The Redmi Note 10 Pro and its Snapdragon 732G score 563 in the same test. The Redmi Note 11 Pro 5G's dominance continues in the multi-core test, as its 1933 score handily surpasses the Redmi Note 10 Pro's score of 1728. 
+From those numbers, the Redmi Note 11 Pro 5G and its Snapdragon 695 appear to have about a 15% CPU performance advantage over the Snapdragon 732G-powered Redmi Note 10 Pro. That gap is mirrored on AnTuTu, as the Redmi Note 11 Pro 5G records a score of 121,603 in the CPU test, versus the Redmi Note 10 Pro's 101,946. Ergo, a 20% edge for the newer phone.
+The Redmi Note 11 Pro 5G continues its dominance on the GPU end, too, as it scores 101,082. The Redmi Note 10 Pro scores 93,022, which translates into an 8% advantage for the Redmi Note 11 Pro 5G. While 3DMark is generally better for GPU performance, it'd be surprising if 3DMark numbers differed wildly from AnTuTU's figures.
+In conclusion, the Snapdragon 695-powered Redmi Note 11 Pro 5G is, indeed, a more powerful device than the Snapdragon 732G-powered Redmui Note 10 Pro. It's not a significant upgrade, though. Either way, that should gladden the hearts of fans, especially after the Redmi Note 11's Snapdragon 680 disappointed in tests versus the Redmi Note 10's Snapdragon 678.        ''',
+    date: DateTime(2022, 02, 14),
+    read: 8,
   ),
 ];
